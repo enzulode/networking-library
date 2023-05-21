5566df3ae40374978f320d5cbed7cb055c321b7e
Vladislav Andreev <andreevvlad443@gmail.com>
2023-05-21 18:09:32 +0300

    removed lombok usage in ResponseMapper & added null-checks using java core library

--- src/main/java/com/enzulode/network/mapper/ResponseMapper.java
+++ src/main/java/com/enzulode/network/mapper/ResponseMapper.java
@@ -2,9 +2,10 @@ package com.enzulode.network.mapper;
 
 import com.enzulode.network.exception.MappingException;
 import com.enzulode.network.model.interconnection.Response;
-import lombok.NonNull;
 import org.apache.commons.lang3.SerializationException;
 import org.apache.commons.lang3.SerializationUtils;
+
+import java.util.Objects;
 
 /**
  * This class converts element response instance into a byte array and in an opposite way
@@ -19,8 +20,11 @@ public final class ResponseMapper
 	 * @return response raw bytes
 	 * @throws MappingException if serialization not succeed
 	 */
-	public static byte[] mapFromInstanceToBytes(@NonNull Response response) throws MappingException
+	public static byte[] mapFromInstanceToBytes(Response response) throws MappingException
 	{
+//		Requiring response instance to be non-null
+		Objects.requireNonNull(response, "Response instance cannot be null");
+
 		try
 		{
 			return SerializationUtils.serialize(response);
@@ -39,8 +43,11 @@ public final class ResponseMapper
 	 * @throws MappingException if deserialization not succeed
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T extends Response> T mapFromBytesToInstance(@NonNull byte[] bytes) throws MappingException
+	public static <T extends Response> T mapFromBytesToInstance(byte[] bytes) throws MappingException
 	{
+//		Requiring response bytes to be non-null
+		Objects.requireNonNull(bytes, "Response bytes array cannot be null");
+
 		try
 		{
 			return SerializationUtils.deserialize(bytes);
